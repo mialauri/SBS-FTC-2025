c81c386d13e458034ba0b3f905b55438fdf3ba87
MJL <143568509+mialauri@users.noreply.github.com>
2024-12-12 16:20:20 -0500

    slidetest

---
+++ TeamCode/src/main/java/Tests/SlideTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "SlideTest")
+public class SlideTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
